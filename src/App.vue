e243b8d3a0f5f7ee4d6f472a2742f208a885e246
Christian Asilo <128013689+ChristianAsilo@users.noreply.github.com>
2025-07-08 20:49:03 +0800

    feature: Lot Map sharing - Inital map lot vue app - Added Map and dot line - integrate dots into map

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <ThreeMap />
+</template>
+
+<script setup>
+import ThreeMap from './components/ThreeMap.vue'
+</script>
+
+<style>
+body {
+  margin: 0;
+  overflow: hidden;
+}
+</style>
